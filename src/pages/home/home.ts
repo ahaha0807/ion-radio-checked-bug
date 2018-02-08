db558999690a83bb94c76c3728c78ce977850e7c
ahaha0807 <b5331@oic.jp>
2018-02-09 04:23:40 +0900

    copy and paste and test run and commit from https://ionicframework.com/docs/api/components/radio/RadioButton/ of usage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from '@angular/core'
+import { NavController } from 'ionic-angular'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  relationship: any
+  isDisabled = true
 
   constructor(public navCtrl: NavController) {
 
